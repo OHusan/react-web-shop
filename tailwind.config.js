a10a10263550e23f36627837a71b129e7eb47c36
Husan Omerinović <61432235+OHusan@users.noreply.github.com>
2024-09-02 09:14:47 +0200

    Feature/header (#1)
    * add: header
    
    * add: header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Nunito', ...defaultTheme.fontFamily.sans]
-      }
+        nunito: ['Nunito', ...defaultTheme.fontFamily.sans],
+        newAmsterdam: ['New Amsterdam', 'Poppins', 'sans-serif'],
+      },
+      container: {
+        padding: '1rem',
+      },
     },
   },
   plugins: [],
-}
+};
