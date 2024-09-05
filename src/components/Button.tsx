612394d8f2085092a1f9888a7aa64527e5e212c6
OHusan <omerinovic27@gmail.com>
2024-09-05 11:06:54 +0200

    add categoreis section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
-export const Button: React.FC<{text: string}> = ({text}) => {
+export const Button: React.FC<{text: string, primary?: boolean}> = ({text, primary = false}) => {
   return (
-    <button className="py-3 px-6 flex items-center gap-2.5 bg-primary rounded-lg">
+    <button className={`py-3 px-6 flex items-center gap-2.5 ${primary ? 'bg-primary' : 'bg-white'} rounded-lg`}>
       {text}
       <img src="/arrow-right.svg" alt="Arrow right" className="w-6 h-5"/>
     </button>
